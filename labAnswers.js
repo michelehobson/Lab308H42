e0805085d5922c331684dfef4064a461638cbf47
michelehobson <michelehobson@hotmail.com>
2023-10-14 12:19:45 -0400

    Yell at the Ninja Turtles answered

--- labAnswers.js
+++ labAnswers.js
@@ -31,21 +31,26 @@
 ///////////////////
 // WILD WILD LIFE
 ///////////////////
-const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
-const sharky = ["Sharky", "shark", 20, "Left Coast"]
-const plantee = ["Plantee", "plant",  5000 , "Mordor"]
-const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
-const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
+// const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+// const sharky = ["Sharky", "shark", 20, "Left Coast"]
+// const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+// const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+// const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
 
-plantee[2] += 1;
-wolfy[3] = 'Gotham City';
-dart[0] += ' Hawkins'
-wolfy.shift();
-wolfy.unshift('Gameboy')
-console.log(wolfy + '\n' + sharky + '\n' + plantee + '\n' + porgee + '\n' + dart);
+// plantee[2] += 1;
+// wolfy[3] = 'Gotham City';
+// dart[0] += ' Hawkins'
+// wolfy.shift();
+// wolfy.unshift('Gameboy')
+// console.log(wolfy + '\n' + sharky + '\n' + plantee + '\n' + porgee + '\n' + dart);
+
 ///////////////////
 // Yell at the Ninja Turtles
 ///////////////////
+let turtles = ['Donatello', 'Leonardo', 'Raphael', 'Michaelangelo'];
+for (const ninja of turtles) {
+    console.log(ninja.toUpperCase());
+}
 
 ///////////////////
 // Methods, Revisited

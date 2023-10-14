e3c20a5377114db90c106c001f504f78f970e8d8
michelehobson <michelehobson@hotmail.com>
2023-10-14 12:03:57 -0400

    Wild Wild Life answered

--- labAnswers.js
+++ labAnswers.js
@@ -17,17 +17,80 @@
 ///////////////////
 // FIZZ BUZZ
 ///////////////////
-for (let c = 1; c <= 100; c++) {
-    if (c % 3 === 0 && c % 5 === 0) {
-        console.log('FizzBuzz');
-    } else if (c % 3 === 0) {
-        console.log('Fizz');
-    } else if (c % 5 === 0) {
-        console.log('Buzz');
-    } else {
-        console.log(c);
-    }
-}
+// for (let c = 1; c <= 100; c++) {
+//     if (c % 3 === 0 && c % 5 === 0) {
+//         console.log('FizzBuzz');
+//     } else if (c % 3 === 0) {
+//         console.log('Fizz');
+//     } else if (c % 5 === 0) {
+//         console.log('Buzz');
+//     } else {
+//         console.log(c);
+//     }
+// }
 ///////////////////
 // WILD WILD LIFE
+///////////////////
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+const sharky = ["Sharky", "shark", 20, "Left Coast"]
+const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
+
+plantee[2] += 1;
+wolfy[3] = 'Gotham City';
+dart[0] += ' Hawkins'
+wolfy.shift();
+wolfy.unshift('Gameboy')
+console.log(wolfy + '\n' + sharky + '\n' + plantee + '\n' + porgee + '\n' + dart);
+///////////////////
+// Yell at the Ninja Turtles
+///////////////////
+
+///////////////////
+// Methods, Revisited
+///////////////////
+
+///////////////////
+// Where is Waldo
+///////////////////
+
+///////////////////
+// Excited Kitten
+///////////////////
+
+///////////////////
+// Find the Median
+///////////////////
+
+///////////////////
+// Hungry for More?
+///////////////////
+
+///////////////////
+// Alien Attire
+///////////////////
+
+///////////////////
+// Dress Us Up
+///////////////////
+
+///////////////////
+// Dirty Laundry
+///////////////////
+
+///////////////////
+// Inventory
+///////////////////
+
+///////////////////
+// 
+///////////////////
+
+///////////////////
+// 
+///////////////////
+
+///////////////////
+// 
 ///////////////////

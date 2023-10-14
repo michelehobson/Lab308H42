c3b34c7abbbd44e0b3f6858d9ec0a5119b9c7c6a
michelehobson <michelehobson@hotmail.com>
2023-10-14 11:49:01 -0400

    Fizz Buzz answered

--- labAnswers.js
+++ labAnswers.js
@@ -1,22 +1,33 @@
 ///////////////////
 // EASY GOING
 ///////////////////
-for(let a = 1; a < 21; a++) {
-    console.log(a);
-}
+// for (let a = 1; a < 21; a++) {
+//     console.log(a);
+// }
 
 ///////////////////
 // GET EVEN 
 ///////////////////
-for(let b = 0; b <= 200; b++) {
-    if(b % 2 === 0) {
-        console.log(b);
-    }
-}
+// for (let b = 0; b <= 200; b++) {
+//     if (b % 2 === 0) {
+//         console.log(b);
+//     }
+// }
 
 ///////////////////
 // FIZZ BUZZ
 ///////////////////
+for (let c = 1; c <= 100; c++) {
+    if (c % 3 === 0 && c % 5 === 0) {
+        console.log('FizzBuzz');
+    } else if (c % 3 === 0) {
+        console.log('Fizz');
+    } else if (c % 5 === 0) {
+        console.log('Buzz');
+    } else {
+        console.log(c);
+    }
+}
 ///////////////////
 // WILD WILD LIFE
 ///////////////////
